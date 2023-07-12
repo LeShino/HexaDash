6db5e5df1eaa9ebbec6d991cc0b167a377726d1c
Laubet Guy-Gaël <48186190+LeShino@users.noreply.github.com>
2023-07-12 15:25:18 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
 
-createApp(App).use(store).use(router).mount('#app')
+//// Importation styles Theme Hexadash
+import '@/assets/css/bootstrap.css'
+import '@/assets/css/hexastyles.css'
+
+//// Importation  styles package PrimeVue
+/// Core PrimeVue
+import "primevue/resources/primevue.min.css"
+/// Theme PrimeVue
+// import "primevue/resources/themes/lara-light-indigo/theme.css";
+
+//// Fonts, Icons css
+import '@/assets/icons/unicons-iconscout.css'
+import '@/assets/icons/line-awesome.min.css'
+
+////  Plugins Javascript Theme
+import jquery from 'jquery'
+import $ from 'jquery'
+window.$ = window.jQuery = require('jquery');
+import '@/assets/js/jquery.min.js'
+import '@/assets/js/jqueryui.js'
+import '@/assets/js/popper.js'
+import '@/assets/js/bootstrap.min.js'
+// import '@/assets/js/accordion.js'
+// import '@/assets/js/drawer.js'
+// import'@/assets/js/popover.js'
+import '@/assets/js/main.js'
+
+createApp(App).use(store).use(router).use(PrimeVue).mount('#app')
